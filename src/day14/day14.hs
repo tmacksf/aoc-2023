590a3b59d2f9fb45a4fdb2fe9f8ad04e9c33fc7b
Tommy <tmackcsb@gmail.com>
2023-12-16 18:43:43 +0000

    Day 14 pt 1

--- src/day14/day14.hs
+++ src/day14/day14.hs
@@ -4,7 +4,32 @@ readInputPt1 :: FilePath -> IO [String]
 readInputPt1 fname = lines <$> readFile fname
 
 moveRocks :: [String] -> [String]
-moveRocks xs = singleIter
+moveRocks xs = if si == xs then si else moveRocks si
+  where si = singleIter xs
+
+singleIter :: [String] -> [String]
+singleIter [] = []
+singleIter [x] = [x]
+singleIter (x:y:xs) = fst sr : singleIter ((snd sr) : xs)
+  where sr = singleMove x y 
+
+singleMove :: String -> String -> (String, String)
+singleMove s1 s2 = (
+  (map (\(x, y) -> if canMove x y then 'O' else x) $ zip s1 s2), 
+  (map (\(x, y) -> if canMove x y then '.' else y) $ zip s1 s2))
+
+canMove :: Char -> Char -> Bool
+canMove top bot
+  | top == '.' && bot == 'O' = True
+  | otherwise = False
+
+part1 :: [String] -> Int
+part1 = calcTotal . moveRocks
+
+calcTotal :: [String] -> Int
+calcTotal [] = 0
+calcTotal (x:xs) = length (x:xs) * (sum [1 | y <- x, y == 'O']) + (calcTotal xs)
 
 main = do
-  print "test"
+  x <- readInputPt1 "input.txt"
+  print $ part1 x
